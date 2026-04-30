0d897e78de3fbca5960994f5f77ec86c56cc1381
Nicolas ignacio topa <nachotopa462005@gmail.com>
2026-04-30 11:48:04 +0200

    arreglo backend compatible conn vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 import { app } from './app.js'
 import { config } from './config/index.js'
 
-app.listen(config.port, () => {
-  console.log(`Server running on http://localhost:${config.port}`)
-})
+export default app
+
+if (!process.env.VERCEL) {
+  app.listen(config.port, () => {
+    console.log(`Server running on http://localhost:${config.port}`)
+  })
+}
